d7300083fd2132c6c3d0e0b00b2e2c68f30a84dd
alex <8379108+Alex33856@users.noreply.github.com>
2026-06-09 16:52:45 -0400

    fix: removing recipes from favorites being annoying, make recipes more obvious

--- src/main/kotlin/com/operationpotato/itemlist/gui/favorites/FavoritesListWidget.kt
+++ src/main/kotlin/com/operationpotato/itemlist/gui/favorites/FavoritesListWidget.kt
@@ -1,5 +1,6 @@
 package com.operationpotato.itemlist.gui.favorites
 
+import com.operationpotato.itemlist.Keybinds
 import com.operationpotato.itemlist.favorites.FavoritesManager
 import com.operationpotato.itemlist.gui.AbstractItemList
 import com.operationpotato.itemlist.gui.StackDisplay
@@ -8,8 +9,21 @@ import com.operationpotato.itemlist.utils.RepoLibUtils.toItem
 import com.operationpotato.itemlist.utils.SkyBlockItemCategory
 import com.operationpotato.itemlist.utils.SkyBlockMobsRepo
 import com.operationpotato.itemlist.utils.Utils.toLazy
+import net.minecraft.client.input.KeyEvent
+import tech.thatgravyboat.skyblockapi.helpers.McClient
+import kotlin.jvm.optionals.getOrNull
 
 class FavoritesListWidget(width: Int, height: Int) : AbstractItemList(width, height) {
+
+	override fun keyPressed(event: KeyEvent): Boolean {
+		val mousePos = McClient.mouse
+		val child = getChildAt(mousePos.first, mousePos.second).getOrNull()
+		if (child is RecipeStackDisplay && Keybinds.favoriteItem.matches(event)) {
+			FavoritesManager.removeFavoriteRecipe(child.recipe)
+			return true
+		}
+		return super.keyPressed(event)
+	}
 
 	override fun getItems(): List<StackDisplay> {
 		val displays = mutableListOf<StackDisplay>()
@@ -24,7 +38,7 @@ class FavoritesListWidget(width: Int, height: Int) : AbstractItemList(width, hei
 		FavoritesManager.favorites.favoriteRecipes.forEach { recipe ->
 			val result = recipe.result()?.toItem()
 			if (result != null && !result.isEmpty) {
-				displays.add(StackDisplay(result.toLazy(), SkyBlockItemCategory.ALL))
+				displays.add(RecipeStackDisplay(recipe, result.toLazy(), SkyBlockItemCategory.ALL))
 			}
 		}
 
