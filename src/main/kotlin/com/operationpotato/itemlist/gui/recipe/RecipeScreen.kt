a84fa9b0f7a00a331177371742d3e7fea81c6f97
alex <8379108+Alex33856@users.noreply.github.com>
2026-06-20 12:09:42 -0400

    fix: scrolling direction being reversed

--- src/main/kotlin/com/operationpotato/itemlist/gui/recipe/RecipeScreen.kt
+++ src/main/kotlin/com/operationpotato/itemlist/gui/recipe/RecipeScreen.kt
@@ -141,7 +141,7 @@ class RecipeScreen(val parent: Screen?, val recipes: List<AbstractRecipeWidget>,
 	override fun mouseScrolled(x: Double, y: Double, scrollX: Double, scrollY: Double): Boolean {
 		return if (!super.mouseScrolled(x, y, scrollX, scrollY)) {
 			if (x >= topLayout.x && x <= topLayout.x + topLayout.width) {
-				if (scrollY > 0) goForward() else goBackward()
+				if (scrollY < 0) goForward() else goBackward()
 				return true
 			}
 			false
