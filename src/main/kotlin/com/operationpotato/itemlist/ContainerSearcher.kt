2894eae22b894668a12a0796d03f5dffc174533a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-07-17 19:10:57 +0200

    fix: search disappearing when switching menus

--- src/main/kotlin/com/operationpotato/itemlist/ContainerSearcher.kt
+++ src/main/kotlin/com/operationpotato/itemlist/ContainerSearcher.kt
@@ -1,26 +1,39 @@
 package com.operationpotato.itemlist
 
 import com.operationpotato.itemlist.utils.SearchUtils
+import net.minecraft.world.item.ItemStack
+import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI
+import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent
 import tech.thatgravyboat.skyblockapi.api.item.replaceVisually
 import tech.thatgravyboat.skyblockapi.helpers.McScreen
 
 object ContainerSearcher {
 
+	init {
+		SkyBlockAPI.eventBus.register<InventoryChangeEvent> {
+			if (shouldSearch()) {
+				// Instead of rechecking every item, just recheck the updated item
+				setItemSearch(SkyBlockItemList.instance?.searchBox?.value, it.item)
+			}
+		}
+	}
+
 	fun shouldSearch(): Boolean = SkyBlockItemList.instance?.searchBox?.isSearchingInventory == true
 
 	fun setSearch(search: String?) {
-		val searches = search?.lowercase()?.let { SearchUtils.transformSearch(it) }
+		McScreen.asMenu?.menu?.items?.forEach { item -> setItemSearch(search, item) }
+	}
 
-		McScreen.asMenu?.menu?.items?.forEach { item ->
-			val color = when {
-				searches == null -> 0
-				SearchUtils.matchesSearch(item, searches) -> 0
-				else -> 0xAA555555.toInt()
-			}
-			item.replaceVisually {
-				copyFrom(item)
-				foregroundColor = color
-			}
+	private fun setItemSearch(search: String?, item: ItemStack) {
+		val searches = search?.lowercase()?.let { SearchUtils.transformSearch(it) }
+		val color = when {
+			searches == null -> 0
+			SearchUtils.matchesSearch(item, searches) -> 0
+			else -> 0xAA555555.toInt()
+		}
+		item.replaceVisually {
+			copyFrom(item)
+			foregroundColor = color
 		}
 	}
 
